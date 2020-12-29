3c4cbb5789894d8a80adbb906a203ec16f4cd6e1
Daniel Osineye <daniel.osineye@gmail.com>
2020-12-29 06:07:36 +0100

    receiever-functions

--- cards/oopvsgoaproach/deck.go
+++ cards/oopvsgoaproach/deck.go
@@ -1,1 +1,16 @@
 package main
+
+import "fmt"
+
+type deck []string
+
+func (d deck) print() {
+	for i, item := range d {
+		fmt.Println(i, item)
+	}
+}
+
+// I did not realise I could create type(s) of primitive golang types (as in -> type deck []string)like I'd do with a struct (as in -> type User struct{}), peharps struct is also a golang primitive type
+
+// Notes (Good practice)
+// Always refer to the recievers with 1 or 2 characters that match the type
