82af885b0c35019fa822ee96281f89b7185ce8fd
Daniel Osineye <daniel.osineye@gmail.com>
2020-12-29 06:25:23 +0100

    creating a new deck

---
+++ cards/05 - creatinganewdeck/deck.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func (d deck) print() {
+	for i, item := range d {
+		fmt.Println(i, item)
+	}
+}
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			card := suit + " of " + value
+			cards = append(cards, card)
+		}
+	}
+	return cards
+}
+
+// Notes (Good practice)
+// 1. Only use receivers where necesary e.g when creating a new deck you do not need to use a receiver
