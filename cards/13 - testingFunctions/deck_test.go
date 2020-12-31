0b81bbc426cda047fb62779eb8ab74e0c01d37d1
Daniel Osineye <daniel.osineye@gmail.com>
2020-12-31 04:46:21 +0100

    testing_newDeck

---
+++ cards/13 - testingFunctions/deck_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+// Writing useful tests/deciding what to test
+// testing newDeck
+// 1. Make sure a deck is created with x number of cards
+// 2. Make sure the first card is an Ace of Spades
+// 3 Make sure the last card is a Four of CLubs
+
+// Conditions
+// 1. total number of cards expected => suits * values
+
+// testing saveToFile() & newDeckFromFile()
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v: ", len(d))
+	}
+}
