5757eaa33a29d0ccd9ead8eee8defed791e34994
Daniel Osineye <daniel.osineye@gmail.com>
2021-04-02 07:56:11 +0100

    structsAndPointers

---
+++ structsAndPointers/18 - pointers/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode string
+}
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	daniel := &person{
+		firstName: "Daniel",
+		lastName:  "Osineye",
+		contactInfo: contactInfo{
+			email:   "daniel.osineye@gmail.com",
+			zipCode: "12345",
+		},
+	}
+	daniel.updateFirstname("Danny")
+	daniel.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func (p *person) updateFirstname(newFirstName string) {
+	p.firstName = newFirstName
+}
