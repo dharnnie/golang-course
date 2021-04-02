35d68d231ce717ad66313f1dd63ebe26c2f880e6
Daniel Osineye <daniel.osineye@gmail.com>
2021-04-02 06:10:37 +0100

    practice

--- cards/13 - testingFunctions/deck_test.go
+++ cards/13 - testingFunctions/deck_test.go
@@ -19,9 +19,9 @@ func TestNewDeck(t *testing.T) {
 		t.Errorf("Expected deck length of 16, but got %v: ", len(d))
 	}
 	if d[0] != "Ace of Spades" {
-		t.Errorf("Expected first card of Ace of Spades, but got %v: ", d[0])
+		t.Errorf("Expected first card of Ace of Spades, but got %v: ", len(d))
 	}
 	if d[len(d)-1] != "Four of Clubs" {
-		t.Errorf("Expected last card of Four of Clubs, but got %v: ", d[0])
+		t.Errorf("Expected last card of Four of Clubs, but got %v: ", len(d))
 	}
 }
